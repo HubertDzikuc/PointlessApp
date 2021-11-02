c3967b8c2ed926d872de777913bedef5e7d7a02b
Hubert Dzikuć <hubert.dzikuc@gmail.com>
2021-11-02 11:58:00 +0100

    [Backend] Connection with CosmosDB DataBase

--- backend/GetUserPoints.cs
+++ backend/GetUserPoints.cs
@@ -7,21 +7,41 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
 
 namespace PointlessApp
 {
     public static class GetUserPoints
     {
+        public class User
+        {
+            public string email { get; set; }
+            public int points { get; set; }
+        }
+
         [FunctionName("GetUserPoints")]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "pointlessapp",
+                collectionName: "users",
+                ConnectionStringSetting = "CosmosDBConnection"
+            )] DocumentClient dataBase,
             ILogger log)
         {
             try
             {
                 string email = req.Query["email"];
 
-                return new OkObjectResult(email);
+                var collectionUri = UriFactory.CreateDocumentCollectionUri("pointlessapp", "users");
+
+                var user = dataBase.CreateDocumentQuery<User>(collectionUri)
+                    .Where(p => p.email.Equals(email))
+                    .AsEnumerable()
+                    .FirstOrDefault();
+
+                return new OkObjectResult(user.points);
             }
             catch (System.Exception ex)
             {
