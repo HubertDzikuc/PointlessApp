2894b95dc4154c8c8d802f38cea495260ebdf5c6
Hubert Dzikuć <hubert.dzikuc@gmail.com>
2021-11-02 12:45:38 +0100

    [Backend] GetAllNegativeActivities Init

---
+++ backend/GetAllNegativeActivities.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+using Microsoft.Azure.Documents.Linq;
+using System.Collections.Generic;
+
+namespace PointlessApp
+{
+    public static class GetAllNegativeActivities
+    {
+        public class Activity
+        {
+            public string name { get; set; }
+            public int points { get; set; }
+        }
+
+        [FunctionName("GetAllNegativeActivities")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "pointlessapp",
+                collectionName: "negative-activities",
+                ConnectionStringSetting = "CosmosDBConnection"
+            )] DocumentClient dataBase,
+            ILogger log)
+        {
+            try
+            {
+                var collectionUri = UriFactory.CreateDocumentCollectionUri("pointlessapp", "negative-activities");
+
+                var query = dataBase.CreateDocumentQuery<Activity>(collectionUri)
+                    .AsDocumentQuery();
+
+                var activities = new List<Activity>();
+
+                while (query.HasMoreResults)
+                {
+                    foreach (Activity result in await query.ExecuteNextAsync())
+                    {
+                        activities.Add(result);
+                    }
+                }
+
+                return new OkObjectResult(activities);
+            }
+            catch (System.Exception ex)
+            {
+                return new BadRequestObjectResult(ex);
+            }
+        }
+    }
+}
